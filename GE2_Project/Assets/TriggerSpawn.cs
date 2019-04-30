45255de6afc7bc42d759b7d4316ed6bc841c23c9
AsianBear <c16347416@mydit.ie>
2019-04-30 10:51:37 +0100

    Small Update

---
+++ GE2_Project/Assets/TriggerSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSpawn : MonoBehaviour
+{
+    public GameObject portal;
+    public GameObject[] ships;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        portal.SetActive(true);
+    }
+}
