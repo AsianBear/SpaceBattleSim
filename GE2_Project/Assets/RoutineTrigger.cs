e7ddeff8f508a410f3ab0d5c13f9ce9ba0256800
AsianBear <c16347416@mydit.ie>
2019-05-03 01:32:13 +0100

    Updated Scene4

--- GE2_Project/Assets/RoutineTrigger.cs
+++ GE2_Project/Assets/RoutineTrigger.cs
@@ -7,6 +7,12 @@ public class RoutineTrigger : MonoBehaviour
 
     public GameObject portal;
     public GameObject ships;
+    public GameObject lazer;
+    public GameObject chargeFX;
+    public GameObject electricFX;
+    public GameObject chargeFinished;
+    public GameObject explosion;
+    public GameObject smallExplosion;
 
     void Start()
     {
@@ -21,5 +27,35 @@ public class RoutineTrigger : MonoBehaviour
         ships.SetActive(true);
         yield return new WaitForSeconds(5f);
         portal.SetActive(false);
+        yield return new WaitForSeconds(2.1f);
+        chargeFX.SetActive(true);
+        electricFX.SetActive(true);
+        yield return new WaitForSeconds(7.5f);
+        chargeFX.SetActive(false);
+        electricFX.SetActive(false);
+        chargeFinished.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        chargeFinished.SetActive(false);
+        lazer.SetActive(true);
+    }
+
+    void DestroyThis()
+    {
+        explosion.SetActive(true);
+        Destroy(ships.gameObject);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Projectile"))
+        {
+            DestroyThis();
+            smallExplosion.SetActive(true);
+        }
+        else
+        {
+            smallExplosion.SetActive(false);
+        }
+        
     }
 }
